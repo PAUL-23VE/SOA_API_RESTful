1d535d9ce8f0486dec838b1a41a69ab5ab4c0e4f
DavidGR21 <davidgiler21@gmail.com>
2025-09-12 18:12:54 -0500

    Incorporación de una base de datos con MariaDB

---
+++ Data/DB.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SOA_API_RESTful.Data
+{
+    public static class DB
+    {
+        private static string connectionString = "Server=localhost;Database=soa_db;User=root;Password=tu_password;";
+
+        public static MySqlConnection GetConnection()
+        {
+            var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
