94e14d2420b836d6ee6e14fa19a4a834fb8bddbb
XD_PAUL <130878879+PAUL-23VE@users.noreply.github.com>
2025-09-12 19:09:51 -0500

    Update DB.cs

--- Data/DB.cs
+++ Data/DB.cs
@@ -4,7 +4,7 @@ namespace SOA_API_RESTful.Data
 {
     public static class DB
     {
-        private static string connectionString = "Server=10.79.25.70;Port=3306;Database=soa_db;User=paul;Password=123;";
+        private static string connectionString = "Server=10.79.16.186;Port=3306;Database=soa_db;User=paul;Password=123;";
 
         public static MySqlConnection GetConnection()
         {
